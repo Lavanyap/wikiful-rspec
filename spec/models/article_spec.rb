b9f2a56e68ff744fb13e0e3325700acd561ee2e2
Lavanya <lavanya.puchakayla@gmail.com>
2014-08-13 14:37:22 -0700

    test2

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,10 +5,14 @@ RSpec.describe Article, :type => :model do
       article = Article.new 
       article.should_not be_valid
   end
+ 
+
   it "changes the number od article" do
      category1= Category.new(name: "science")
      category2 = Category.new(name: "Technology")
   	  article = Article.new(title: "recent bioingormatics news", content: "nnnknkn",categories: [category1])
-	  expect {Article.save}.to change {Article.count}.by(1)      
+	  expect {article.save}.to change {Article.count}.by(1)      
     end
+
+    it { should belong_to :user }
 end
